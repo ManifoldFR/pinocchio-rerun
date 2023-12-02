67003eb2e54d0f8eb6ed62c4c765a5177204e3ca
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-02 13:49:31 +0100

    [examples] create directory, add solo8.py

---
+++ examples/solo8.py
@@ -0,0 +1,19 @@
+import pyseptum
+import example_robot_data as erd
+import pinocchio as pin
+import numpy as np
+
+
+robot = erd.load("solo8")
+model = robot.model
+visual_model = robot.visual_model
+rr = pyseptum.RerunVisualizer(model, visual_model)
+rr.initViewer()
+q0 = pin.neutral(model)
+rr.display(q0)
+
+
+def displayRandomConfiguration():
+    q = pin.randomConfiguration(model)
+    q[:3] = np.clip(q[:3], -1., 1.)
+    rr.display(q)
