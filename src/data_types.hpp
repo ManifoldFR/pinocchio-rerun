dceab70e7f77e40ae47fda61c96d82d69f2bc58b
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-02 13:49:31 +0100

    add typedef VectorRef

--- src/data_types.hpp
+++ src/data_types.hpp
@@ -5,6 +5,7 @@
 namespace pinviz {
 
 using Eigen::VectorXd;
+using VectorRef = Eigen::Ref<VectorXd>;
 using ConstVectorRef = Eigen::Ref<const VectorXd>;
 using Eigen::Matrix3Xf;
 using Eigen::Vector3f;
