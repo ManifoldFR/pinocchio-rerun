5ed1a8379474ce8a78061687e6f6bfde2bdff8e5
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-02 13:49:30 +0100

    add src/pinocchio.hpp, src/pinocchio.cpp
    + add "eigen-adapter.hpp" headers eigen Vector3f adapter
    + .clang-format:  change indent size to 2

---
+++ src/pinocchio.cpp
@@ -0,0 +1,35 @@
+#include "./pinocchio.hpp"
+
+#include <pinocchio/multibody/geometry.hpp>
+
+namespace pinviz {
+
+auto loadPinGeometry(const pinocchio::GeometryObject &geom) {
+
+  MeshLoaderPtr ml = std::make_shared<hpp::fcl::MeshLoader>();
+  hpp::fcl::BVHModelPtr_t p = ml->load(geom.meshPath, geom.meshScale);
+
+  return p;
+}
+
+void loadPinocchioModel(const pinocchio::GeometryModel &geomModel,
+                        rerun::RecordingStream &rr) {
+  using GeometryObjectVector = pinocchio::GeometryModel::GeometryObjectVector;
+  const GeometryObjectVector &geomObjects = geomModel.geometryObjects;
+  for (const auto &gobj : geomObjects) {
+    auto p = loadPinGeometry(gobj);
+    auto m = toRerunMesh(p);
+    rr.log("model/" + gobj.name, std::move(m));
+  }
+}
+
+rerun::Mesh3D toRerunMesh(const hpp::fcl::BVHModelPtr_t &ptr) {
+  uint numTris = ptr->num_tris;
+  uint numVtx = ptr->num_vertices;
+
+  // convert down to float
+  std::vector<Eigen::Vector3f> vtx = eigenStdVecCast(*ptr->vertices);
+  return rerun::Mesh3D(vtx);
+}
+
+} // namespace pinviz
---
+++ src/pinocchio.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/multibody/data.hpp>
+#include <hpp/fcl/mesh_loader/loader.h>
+#include <hpp/fcl/BVH/BVH_model.h>
+
+#include "eigen_adapters.hpp"
+
+namespace pinocchio {
+struct GeometryObject;
+}
+
+namespace pinviz {
+
+using MeshLoaderPtr = std::shared_ptr<hpp::fcl::MeshLoader>;
+
+auto loadPinGeometry(const pinocchio::GeometryObject &geom);
+
+void loadPinocchioModel(const pinocchio::GeometryModel &geomModel,
+                        rerun::RecordingStream &rr);
+
+inline auto eigenStdVecCast(const std::vector<Eigen::Vector3d> &container) {
+  std::vector<Eigen::Vector3f> out(container.size());
+  for (size_t i = 0; i < out.size(); i++) {
+    out[i] = container[i].cast<float>();
+  }
+  return out;
+}
+
+rerun::Mesh3D toRerunMesh(const hpp::fcl::BVHModelPtr_t &ptr);
+
+} // namespace pinviz
