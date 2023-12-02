c115a8e1464c5301ec3669508bde6c4339b0b6de
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-02 13:49:30 +0100

    [tests] test_load_mesh : create rerun::RecordingStream and visualize big fluffy mammoth in it

--- tests/test_load_mesh.cpp
+++ tests/test_load_mesh.cpp
@@ -1,18 +1,32 @@
 #include <gtest/gtest.h>
 #include <fmt/core.h>
+#include <rerun.hpp>
 
 #include "./util.hpp"
 #include "load_mesh.hpp"
 
 using namespace pinviz;
 
+const fs::path MAMMOTH_FILE = fs::path(ASSET_DIR) / "mammoth.obj";
+MeshDescription mesh;
+
 TEST(LoadMesh, mammoth) {
 
-  const fs::path MAMMOTH_FILE = fs::path(ASSET_DIR) / "mammoth.obj";
   fmt::print("Mammoth file: {}\n", MAMMOTH_FILE.c_str());
 
-  MeshDescription mesh = loadMesh(MAMMOTH_FILE);
+  mesh = loadMesh(MAMMOTH_FILE);
   fmt::print("Mesh num vertices: {:d}\n", mesh.vertices.size());
   fmt::print("Mesh num normals : {:d}\n", mesh.normals.size());
   fmt::print("Mesh num faces   : {:d}\n", mesh.faceTriangles.size());
+}
+
+TEST(LoadMesh, mammoth_rerun) {
+
+  mesh = loadMesh(MAMMOTH_FILE);
+
+  const rerun::RecordingStream rec("mammoth_example_cpp");
+  rec.spawn().exit_on_failure();
+
+  rerun::Mesh3D rmesh = meshDescriptionToRerun(std::move(mesh));
+  rec.log("mammoth_mesh", std::move(rmesh));
 }
