84b2717d400e24e0a25ff6824585cc772ea0455a
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-02 13:49:30 +0100

    [tests] commit test_load_robot.cpp

---
+++ tests/test_load_robot.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include "RerunVisualizer.hpp"
+#include "./util.hpp"
+
+#include <pinocchio/algorithm/joint-configuration.hpp>
+
+const fs::path ur_descr_path = fs::path(ERD_DIR) / "ur_description";
+
+Robot loadUR(const std::string &robot_name) {
+  fs::path urdfPath = ur_descr_path / "urdf" / (robot_name + ".urdf");
+  if (!fs::exists(urdfPath))
+    throw std::invalid_argument("provided robot " + robot_name +
+                                " is not available.");
+
+  return loadRobotFromURDF(urdfPath, package_dirs_base);
+}
+
+TEST(LoadRobotTest, load) {
+
+  Robot robot = loadUR("ur3_gripper");
+  robot = loadUR("ur5_robot");
+}
+
+TEST(LoadRobotTest, UR3_visualizer) {
+  Robot robot = loadUR("ur3_gripper");
+
+  std::cout << "Model: " << robot.model.name << std::endl;
+  pinviz::RerunVisualizer rr(robot.model, robot.vizModel);
+  rr.stream.set_time_seconds("stable_time", 0.0);
+  rr.initViewer();
+
+  Eigen::VectorXd q0 = pinocchio::neutral(robot.model);
+  rr.display(q0);
+}
